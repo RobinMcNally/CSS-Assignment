191822580d18440e0e92aaf006e72a426009c134
Robin McNally <rlmcnall@gmail.com>
2016-06-14 18:21:50 -0400

    Initial and final commit

---
+++ map.js
@@ -0,0 +1,14 @@
+var mymap = L.map('mapid').setView([42.324271, -85.165130], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'mapbox.streets'
+		}).addTo(mymap);
+
+
+var marker = L.marker([42.327261, -85.180680]).addTo(mymap);
+var marker = L.marker([42.329480, -85.161336]).addTo(mymap);
+var marker = L.marker([42.319863, -85.169716]).addTo(mymap);
